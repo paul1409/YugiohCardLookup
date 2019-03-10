8faa5e52caf1dda78b584274fd1a4a0ba896cd2a
Paul Nguyen <majide9001@gmail.com>
2019-03-10 14:19:56 -0700

    Populated first dropdown

--- listItem.js
+++ listItem.js
@@ -1,3 +1,5 @@
 var cardlist = require('./cardsinfo.json');
-var prop = Object.keys(cardlist.cards[0])
-console.log(Object.keys(prop))
+var prop = Object.keys(cardlist.cards[0]);
+console.log(cardlist.cards[0]);
+var yahooOnly = JSON.parse('./cardsinfo.json');
+console.log(yahooOnly);
