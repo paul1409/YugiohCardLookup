e57fbad09abf8cfd9fc1767674451fa268e82043
Paul Nguyen <majide9001@gmail.com>
2019-03-09 16:56:41 -0800

    Implemented table view

--- listItem.js
+++ listItem.js
@@ -1,14 +1,3 @@
-var cards = require('./cardsinfo.json');
-
-var dropdownContent = document.querySelector('.dropdown-content');
-
-for (i = 0; i < cards.length; i++) {
-
-    var element = cards[i];
-
-    var htmlToAppend = document.createElement('a');
-    htmlToAppend.innerHTML = element.B;
-    htmlToAppend.href = element.C;
-
-    dropdownContent.appendChild(htmlToAppend);
-}
+var cardlist = require('./cardsinfo.json');
+var prop = Object.keys(cardlist.cards[0])
+console.log(Object.keys(prop))
