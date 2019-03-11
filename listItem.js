adaafac381b535fcce9c7993a6cd6e9afad44dde
Paul Nguyen <paul.t.nguyen@sjsu.edu>
2019-03-11 09:51:17 -0700

    Filtered first dropdown list

--- listItem.js
+++ listItem.js
@@ -3,3 +3,6 @@ var prop = Object.keys(cardlist.cards[0]);
 console.log(cardlist.cards[0]);
 var yahooOnly = JSON.parse('./cardsinfo.json');
 console.log(yahooOnly);
+
+
+var ignore = ["id", "name", "desc", "type", "set_tag", "setcode", "image_url", "image_url_small"];
