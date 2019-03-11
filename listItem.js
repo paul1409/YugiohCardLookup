adb3e0576023fb1f27c789621e846195f3d1580b
Paul Nguyen <paul.t.nguyen@sjsu.edu>
2019-03-11 11:30:32 -0700

    Successful filtering

--- listItem.js
+++ listItem.js
@@ -1,8 +1,3 @@
 var cardlist = require('./cardsinfo.json');
 var prop = Object.keys(cardlist.cards[0]);
 console.log(cardlist.cards[0]);
-var yahooOnly = JSON.parse('./cardsinfo.json');
-console.log(yahooOnly);
-
-
-var ignore = ["id", "name", "desc", "type", "set_tag", "setcode", "image_url", "image_url_small"];
